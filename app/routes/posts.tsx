b0e22c0eb5dc490386ee56e7dd58ad54033cf785
ioanatatu <tatu.ioana@gmail.com>
2022-06-26 15:15:53 +0200

    add nested routes

---
+++ app/routes/posts.tsx
@@ -0,0 +1,5 @@
+import { Outlet } from '@remix-run/react';
+
+const Posts = () => <Outlet />;
+
+export default Posts;
---
+++ app/routes/posts/new.tsx
@@ -0,0 +1,5 @@
+function NewPost() {
+  return <div>this is a new post</div>;
+}
+
+export default NewPost;
